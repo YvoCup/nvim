eda36b73c97e9cb8b471396ff118dc425075c2b5
YvoCup <1157661964@qq.com>
2025-11-02 23:44:51 +0800

    git signs highlight change to my color theme

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,13 @@
+----------------------------------------------------- highlight --------------------------------------------------------
+vim.api.nvim_set_hl(0, "GitSignsAdd",          { bg = "none", fg = "#a6d189" })
+vim.api.nvim_set_hl(0, "GitSignsChange",       { bg = "none", fg = "#d9be88" })
+vim.api.nvim_set_hl(0, "GitSignsDelete",       { bg = "none", fg = "#e78284" })
+vim.api.nvim_set_hl(0, "GitSignsChangedelete", { bg = "none", fg = "#eba3a4" })
+vim.api.nvim_set_hl(0, "GitSignsTopdelete",    { bg = "none", fg = "#eba3a4" })
+
+-- 每一行后边的那个的对照
+vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { fg = "#8f9ac2", bold = true })
+
 return {
   "lewis6991/gitsigns.nvim",
   config = function ()
@@ -27,7 +37,7 @@ return {
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        virt_text_pos = "eol", -- "eol" | "overlay" | "right_align"
         delay = 100,
         ignore_whitespace = false,
         virt_text_priority = 100,
