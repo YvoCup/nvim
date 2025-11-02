d2fc5611aa1f9798e75a2d0d41eb3de089d155aa
YvoCup <1157661964@qq.com>
2025-11-02 23:29:31 +0800

    fix lazygit nvim for telescpoe

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,18 @@
+return {
+  "kdheepak/lazygit.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+}
