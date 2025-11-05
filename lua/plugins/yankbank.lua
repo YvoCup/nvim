3081fbd57608a8212ea44f759a6b2d539dde5642
YvoCup <1157661964@qq.com>
2025-11-06 00:50:31 +0800

    add new plugin yankbank

---
+++ lua/plugins/yankbank.lua
@@ -0,0 +1,21 @@
+return {
+  "ptdewey/yankbank-nvim",
+  dependencies = "kkharji/sqlite.lua",
+  lazy = false,
+  cmd = { "YankBank" },
+  config = function()
+    require('yankbank').setup({
+      persist_type = "sqlite",
+      num_behavior = "jump",
+      keymaps = {
+        paste = "<CR>",
+        paste = "p",
+        paste_back = "P",  -- 粘贴在当前行的上方
+      },
+    })
+  end,
+  keys = {
+    { "<leader>p", "<cmd>YankBank<cr>",  mode = "n", desc = "open yank bank" },
+    { "<leader><s-p>", "<cmd>YankBankClearDB<cr>",  mode = "n", desc = "clear yank bank data-base" },
+  }
+}
