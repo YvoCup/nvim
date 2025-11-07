740f0409c84f6c81a48702cba94da96721831f1f
YvoCup <1157661964@qq.com>
2025-11-08 00:50:07 +0800

    add some todo type, add set more keymaps

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,13 +1,17 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  lazy = false,
+  -- 设置为所有的工作都完成之后再去执行的
+  event = "VeryLazy",
   config = function()
     require("todo-comments").setup({
+      highlight = {
+        priority = 200
+      },
       keywords = {
-        -- INFO: 更为基础的标签了
+        -- INFO: 简单基础的介绍，主要介绍基本的功能
         INFO = { icon = " ", color = "#79bdb3" },
-        -- WARN: 警告部分，警告用法或者写法，但不要和别的地方弄混了
+        -- WARN: 警告部分，警告用法或者写法
         WARN = { icon = " ", color = "#e5c890" },
         -- BUGS: 这是一个 bug
         BUGS = { icon = " ", color = "#c1c8e6" },
@@ -23,6 +27,10 @@ return {
         TEST = { icon = "󰂓 ", color = "#8caaee" },
         -- MARK: 标记，当需要寻找的时候快速查找
         MARK = { icon = "󰓹 ", color = "#f4b8e4" },
+        -- PARA: 函数或者别的什么的参数高亮提示
+        PARA = { icon = " ", color = "#dd74dd" },
+        -- FILE: 文件名
+        FILE = { icon = " ", color = "#81c8be" },
       },
     })
   end,
@@ -32,7 +40,7 @@ return {
       "<leader>tj",
       function()
         require("todo-comments").jump_next({
-          keywords = { "TODO", "ERROR", "WARN", "HACK", "OPTM", "TEST", "BUGS", "DEAD", "MARK" }
+          keywords = { "TODO", "WARN", "HACK", "OPTM", "TEST", "BUGS", "DEAD", "MARK", "INFO" }
         })
       end,
       desc = "next error/warning todo comment"
@@ -42,13 +50,38 @@ return {
       "<leader>tk",
       function()
         require("todo-comments").jump_prev({
-          keywords = { "TODO", "ERROR", "WARN", "HACK", "OPTM", "TEST", "BUGS", "DEAD", "MARK" }
+          keywords = { "TODO", "WARN", "HACK", "OPTM", "TEST", "BUGS", "DEAD", "MARK", "INFO" }
         })
       end,
       desc = "previous error/warning todo comment",
     },
-    { "<leader>tt", "<cmd>TodoTelescope<cr>",  mode = "n", desc = "toggle todo telescope" },
-    { "<leader>tq", "<cmd>TodoQuickFix<cr>",   mode = "n", desc = "toggle todo Quickfix"  },
-    { "<leader>tl", "<cmd>TodoLocList<cr>",    mode = "n", desc = "toggle todo Local list" },
+    -- telescope 查阅，不会列出一般信息类选项
+    {
+      mode = { "n" },
+      "<leader>tt",
+      "<cmd>TodoTelescope keywords=TODO,WARN,OPTM,BUGS,DEAD,MARK,INFO<cr>",
+      desc = "toggle todo telescope"
+    },
+    -- 列出几个关键的需要快速修复的问题
+    { 
+      mode = { "n" },
+      "<leader>tq",
+      "<cmd>TodoQuickFix keywords=TODO,BUGS,OPTM<cr>",
+      desc = "toggle todo Quickfix"
+    },
+    -- 除了一般信息类选项之外都会列举出来，相当于 buffer 形式的 telescope
+    {
+      mode = { "n" },
+      "<leader>tl",
+      "<cmd>TodoLocList keywords=TODO,WARN,OPTM,BUGS,DEAD,MARK,INFO<cr>",
+      desc = "toggle todo Local list"
+    },
+    -- 专门针对 MRAK 做一层标记查阅
+    {
+      mode = { "n" },
+      "<leader>tm",
+      "<cmd>TodoLocList keywords=MARK<cr>",
+      desc = "toggle mark lists"
+    },
   },
 }
